fba178edb8f101a599500ede5ed5824536447bf3
Artyom G <agasparyan0000@gmail.com>
2023-08-01 21:00:33 +0400

    https

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 console.clear()
 
 let express = require('express')
-const cors = require('cors')
+let cors = require('cors')
 let mongoose = require('mongoose')
 let bodyParser = require('body-parser')
+let https = require("https");
+let fs = request("fs");
+
 
 require('dotenv').config({ path: `.env.local`, override: true })
 
@@ -16,12 +19,22 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+const path = __dirname + '/app/views/';
+app.use(express.static(path));
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+
+
 app.use('/users/', usersRoutes)
 app.use('/youtube-accounts/', youtubeAccountsRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log(`[success] server live on port ${process.env.PORT}`)
-})
+https
+.createServer(app)
+.listen(process.env.PORT, ()=>{
+  console.log(`[success] server live on port ${process.env.PORT}`)
+});
 
 if (process.env.DB_URL) {
     mongoose.connect(process.env.DB_URL).then(() => {
